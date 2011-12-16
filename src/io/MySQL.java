6c8969f32d35b8a481889cfcac5bdea5d5d9b626
dozedoff <dozedoffagain@gmail.com>
2011-12-16 21:00:17 +0100

    moved Aid specific code to a new class io.MySQLaid.java in the aid project.

--- src/io/MySQL.java
+++ src/io/MySQL.java
@@ -18,7 +18,6 @@ import java.awt.Image;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.InvalidParameterException;
 import java.sql.Blob;
@@ -32,21 +31,16 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Properties;
 import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
-
-import filter.FilterItem;
-import filter.FilterState;
-
 /**
  * Class for database communication.
  */
 public class MySQL{
 	HashMap<String, PreparedStatement> prepStmts = new HashMap<String, PreparedStatement>();
-	private static Logger logger = Logger.getLogger(MySQL.class.getName());
+	protected static Logger logger = Logger.getLogger(MySQL.class.getName());
 	Connection cn = null;
 	Properties mySqlProps;
 	int retries = 0;
@@ -203,40 +197,7 @@ public class MySQL{
 		return res;
 	}
 
-	public boolean addFilter(FilterItem fi){
-		return addFilter(fi.getUrl().toString(), fi.getBoard(), fi.getReason(), fi.getState());
-	}
-
-	/**
-	 * Adds a filter item to the database.
-	 * @param id id of the item
-	 * @param board board alias
-	 * @param reason reason for adding the filter
-	 * @param state initial state of the filter
-	 * @return true if the filter was added, else false
-	 */
-	public boolean addFilter(String id, String board, String reason, FilterState state){
-		reconnect();
-		try {
-			PreparedStatement addFilter = getPrepStmt("addFilter");
-			addFilter.setString(1, id);
-			addFilter.setString(2, board);
-			addFilter.setString(3, reason);
-			addFilter.setShort(4, (short) state.ordinal());
-			int res = addFilter.executeUpdate();
 	
-			if(res == 0){
-				logger.warning("filter already exists!");
-				return false;
-			}else{
-				return true;
-			}
-		} catch (SQLException e) {
-			logger.warning("MySql Filter add failed: "+e.getMessage());
-		}
-	
-		return false;
-	}
 
 	/**
 	 * Add the current URL to the cache, or update it's Timestamp if it
@@ -313,67 +274,6 @@ public class MySQL{
 		ps.execute();
 	}
 
-	public void updateState(String id, FilterState state){
-		reconnect();
-		PreparedStatement updateFilter = getPrepStmt("updateFilter");
-		try {
-			updateFilter.setShort(1, (short)state.ordinal());
-			updateFilter.setString(2, id);
-			updateFilter.executeUpdate();
-		} catch (SQLException e) {
-			logger.warning("MySql filter update failed: "+e.getMessage());
-		}
-	}
-
-	public FilterState getFilterState(String id){
-		reconnect();
-		ResultSet rs;
-
-		try {
-			getPrepStmt("filterState").setString(1, id);
-			rs = prepStmtQuery("filterState");
-			if(!rs.first()){
-				rs.close();
-				return FilterState.UNKNOWN;
-			}
-			else{
-				FilterState fs = FilterState.values()[(int)rs.getShort(1)];
-				rs.close();
-				return fs; 
-			}
-		} catch (SQLException e) {
-			logger.warning("MySql filterstate get failed: "+e.getMessage());
-		}
-		return FilterState.UNKNOWN;
-	}
-
-	/**
-	 * Returns all items in the filter with state set to pending (1).
-	 * @return a list of all pending filter items
-	 */
-	public LinkedList<FilterItem> getPendingFilters(){
-		reconnect();
-		PreparedStatement pendingFilter = getPrepStmt("pendingFilter");
-		ResultSet rs;
-
-		try {
-			rs = pendingFilter.executeQuery();
-			LinkedList<FilterItem> result = new LinkedList<FilterItem>();
-			while(rs.next()){
-				URL url;
-				url = new URL(rs.getString("id"));
-
-				result.add(new FilterItem(rs.getString("board"), rs.getString("reason"), url, FilterState.PENDING));
-			}
-			rs.close();
-			return result;
-		} catch (SQLException e) {
-			logger.warning("MySql PendingFilter lookup failed: "+e.getMessage());
-		} catch (MalformedURLException e) {
-			logger.warning("Unable to create URL "+e.getMessage());
-		}
-		return new LinkedList<FilterItem>();
-	}
 
 	/**
 	 * Get the number of pending filter items.
