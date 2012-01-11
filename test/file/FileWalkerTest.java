583f17e909b05f52ebb46f92941b4f540e9a3da9
dozedoff <dozedoffagain@gmail.com>
2012-01-11 21:12:38 +0100

    added missing import

--- test/file/FileWalkerTest.java
+++ test/file/FileWalkerTest.java
@@ -9,6 +9,7 @@ import static org.junit.matchers.JUnitMatchers.hasItems;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -98,6 +99,117 @@ public class FileWalkerTest {
 	 * Read all files.
 	 */
 	public void testFullWalk() {
+		fw.addPath(rootFolder);
+		fw.setnoSub(false);
+		LinkedList<File> results = fw.fileWalk();
+		assertThat(results.size(), is(4));
+		
+		assertThat(results, hasItems(files));
+	}
+	@Test
+	/**
+	 * Read all files.
+	 */
+	public void testFullWalkList() {
+		fw.addPath(rootFolder);
+		fw.setnoSub(false);
+		List<File> results = fw.fileWalkList();
+		assertThat(results.size(), is(4));
+		
+		assertThat(results, hasItems(files));
+	}
+	
+	@Test
+	/**
+	 * Read all files.
+	 */
+	public void testFullWalkStringList() {
+		fw.addPath(rootFolder);
+		fw.setnoSub(false);
+		List<String> results = fw.fileWalkStringList();
+		assertThat(results.size(), is(4));
+		
+		String[] filesAsStrings = new String[files.length];
+		
+		for(int i=0; i<files.length; i++){
+			filesAsStrings[i] = files[i].toString();
+		}
+		
+		assertThat(results, hasItems(filesAsStrings));
+	}
+	
+	@Test
+	/**
+	 * Read all files.
+	 */
+	public void testFullWalkString() {
+		fw.addPath(rootFolder.toString());
+		fw.setnoSub(false);
+		LinkedList<File> results = fw.fileWalk();
+		assertThat(results.size(), is(4));
+		
+		assertThat(results, hasItems(files));
+	}
+	
+	@Test
+	/**
+	 * Read files from two select folders
+	 */
+	public void testSelectFolderWalk() {
+		File[] dirsToSearch = new File[2];
+		dirsToSearch[0] = folders[0];
+		dirsToSearch[1] = folders[2];
+		
+		fw.addPath(dirsToSearch);
+		fw.setnoSub(false);
+		LinkedList<File> results = fw.fileWalk();
+		assertThat(results.size(), is(2));
+		
+		assertThat(results, hasItem(files[1]));
+		assertThat(results, hasItem(files[3]));
+	}
+	
+	@Test
+	/**
+	 * Read files from two select folders
+	 */
+	public void testSelectFolderWalkList() {
+		LinkedList<File> list = new LinkedList<>();
+		list.add(folders[0]);
+		list.add(folders[2]);
+		fw.addPath(list);
+		fw.setnoSub(false);
+		LinkedList<File> results = fw.fileWalk();
+		assertThat(results.size(), is(2));
+		
+		assertThat(results, hasItem(files[1]));
+		assertThat(results, hasItem(files[3]));
+	}
+	
+	@Test
+	/**
+	 * Read files from two select folders
+	 */
+	public void testSelectFolderWalkString() {
+		String[] dirsToSearch = new String[2];
+		dirsToSearch[0] = folders[0].toString();
+		dirsToSearch[1] = folders[2].toString();
+		
+		fw.addPath(dirsToSearch);
+		fw.setnoSub(false);
+		LinkedList<File> results = fw.fileWalk();
+		assertThat(results.size(), is(2));
+		
+		assertThat(results, hasItem(files[1]));
+		assertThat(results, hasItem(files[3]));
+	}
+	
+	@Test
+	/**
+	 * Read all files.
+	 */
+	public void testCheckDuplicateAdd() {
+		fw.addPath(rootFolder);
 		fw.addPath(rootFolder);
 		fw.setnoSub(false);
 		LinkedList<File> results = fw.fileWalk();
