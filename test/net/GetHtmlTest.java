202f2a31707911a2bed299532c439f8b0fc8ac62
dozedoff <dozedoffagain@gmail.com>
2012-03-29 22:27:30 +0200

    added embedded Jetty test server

--- test/net/GetHtmlTest.java
+++ test/net/GetHtmlTest.java
@@ -17,17 +17,51 @@ package net;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GetHtmlTest {
-String refData = "<html><head><title>Test Page</title></head><body><p>Test Page</p></body></html>";
+static String refData = "<html><head><title>Test Page</title></head><body><p>Test Page</p></body></html>";
 String testString = null;
+static Server server;
 
-	@Test
+@BeforeClass
+public static void startServer() throws Exception{
+	server  = new Server(80);
+	server.setHandler(new TestHandler());
+	server.start();
+}
+
+@AfterClass
+public static void stopServer() throws Exception{
+	server.stop();
+}
+
+static class TestHandler extends AbstractHandler{
+	@Override
+	public void handle(String arg0, Request baseRequest, HttpServletRequest arg2,
+			HttpServletResponse response) throws IOException, ServletException {
+		response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println(refData);
+	}
+}
+	@Test(timeout=2000)
 	public void testGetString() throws Exception {
 		GetHtml getHtml = new GetHtml();
 		
-		testString = getHtml.get("https://home.zhaw.ch/~wrighnic/test.html");
+		testString = getHtml.get("http://localhost/");
 		assertTrue(testString.equals(refData));
 	}
 }
