4f688d4cf3d7a71cb8e9592ec5fb5a721050e50c
dozedoff <dozedoffagain@gmail.com>
2012-06-01 16:38:29 +0200

    added test for hashing directory

--- test/file/DirectoryHasherTest.java
+++ test/file/DirectoryHasherTest.java
@@ -17,21 +17,22 @@ public class DirectoryHasherTest {
 	byte[] testData2 = {99,21,6,45,12}; // SHA-256: 20FC038E00E13585E68E7EBE50D79CBE7D476A74D8FDE71872627DA6CD8FC8BB
 	
 	LinkedBlockingQueue<FileInfo> fileQueue;
-	DirectoryHasher dh = new DirectoryHasher(fileQueue);
+	DirectoryHasher dh;
 	
 	@Before
 	public void setUp() throws Exception {
 		tempDir  = Files.createTempDirectory("DirectoryHasherTest").toFile();
-		bfr.write(testData, new File(tempDir,"testFile1").toString());
-		bfr.write(testData2, new File(tempDir,"testFile2").toString());
+		bfr.write(testData, new File(tempDir,"testFile1.txt").toString());
+		bfr.write(testData2, new File(tempDir,"testFile2.txt").toString());
 		
 		fileQueue  = new LinkedBlockingQueue<>();
+		dh = new DirectoryHasher(fileQueue);
 	}
 
 	@Test
 	public void testHashDirectory() throws Exception {
 		dh.hashDirectory(tempDir.toString());
-		
+		Thread.sleep(2000);
 		assertThat(fileQueue.size(), is(2));
 	}
 
