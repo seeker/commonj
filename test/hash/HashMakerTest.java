96593622116a5c0cd14e319f45aeffc9117fc4d4
dozedoff <dozedoffagain@gmail.com>
2012-07-01 10:18:55 +0200

    updated tests, separated multiple asserts into own tests

--- test/hash/HashMakerTest.java
+++ test/hash/HashMakerTest.java
@@ -33,8 +33,12 @@ public class HashMakerTest {
 	}
 
 	@Test
-	public void testHash() {
+	public void testHash1() {
 		assertThat(hm.hash(testData), is("95F6A79D2199FC2CFA8F73C315AA16B33BF3544C407B4F9B29889333CA0DB815"));
+	}
+	
+	@Test
+	public void testHash2() {
 		assertThat(hm.hash(testData2), is("20FC038E00E13585E68E7EBE50D79CBE7D476A74D8FDE71872627DA6CD8FC8BB"));
 	}
 	
@@ -60,7 +64,7 @@ public class HashMakerTest {
 	}
 	
 	@Test
-	public void testEmptyDataFile() throws Exception {
+	public void testHashFile3() throws Exception {
 		assertThat(hm.hashFile(testFile3), is("E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855"));
 	}
 	
