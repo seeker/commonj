415375958d66c777e3f601c07916ee8827f946e6
dozedoff <dozedoffagain@gmail.com>
2012-07-30 15:50:32 +0200

    moved classes from ArchiveIndexer

---
+++ src/io/StreamGobbler.java
@@ -0,0 +1,39 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+
+/**
+ * Class for consuming data provided by inputstreams.
+ * 
+ * Based on "When Runtime.exec() won't" from
+ * http://www.javaworld.com/javaworld/jw-12-2000/jw-1229-traps.html
+ */
+public class StreamGobbler extends Thread {
+	InputStream is;
+	String type;
+	final static Logger logger = Logger.getLogger(StreamGobbler.class.getName());
+
+	StreamGobbler(InputStream is, String type) {
+		this.is = is;
+		this.type = type;
+	}
+
+	public void run() {
+		try {
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String line = null;
+			while ((line = br.readLine()) != null)
+				if (type.toLowerCase().equals("error"))
+					logger.warning(type + ">" + line);
+				else
+					logger.info(type + ">" + line);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+}
---
+++ src/thread/ProcessWatchDog.java
@@ -0,0 +1,45 @@
+/*  Copyright (C) 2012  Nicholas Wright
+	
+	part of 'AidUtil', a collection of maintenance tools for 'Aid'.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package thread;
+
+import java.util.logging.Logger;
+
+class ProcessWatchDog extends Thread{
+	String processDescription;
+	Process process;
+	long timeout;
+	final static Logger logger = Logger.getLogger(ProcessWatchDog.class.getName());
+	
+	public ProcessWatchDog(String processDescription, Process process, long timeout) {
+		this.process = process;
+		this.timeout = timeout;
+		this.processDescription = processDescription;
+	}
+
+	@Override
+	public void run() {
+		try {
+			sleep(timeout);
+		} catch (InterruptedException e) {
+			return; // all is well
+		}
+		
+		logger.severe("Process timed out. Description: " + processDescription);
+		process.destroy();
+	}
+}
