47ba3d4490ad74203426660a62b5ab74359cee4b
dozedoff <dozedoffagain@gmail.com>
2012-08-04 13:30:16 +0200

    updated license

--- src/file/FilenameFilterVisitor.java
+++ src/file/FilenameFilterVisitor.java
@@ -1,6 +1,4 @@
 /*  Copyright (C) 2012  Nicholas Wright
-	
-	part of 'AidUtil', a collection of maintenance tools for 'Aid'.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- src/net/DownloadItem.java
+++ src/net/DownloadItem.java
@@ -1,6 +1,4 @@
 /*  Copyright (C) 2012  Nicholas Wright
-	
-	part of 'Aid', an imageboard downloader.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
