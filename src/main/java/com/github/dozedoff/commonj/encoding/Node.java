73f8b0b5d51f6245c8ca6fec9589fcb596e42479
dozedoff <dozedoffagain@gmail.com>
2013-03-12 22:32:36 +0100

    Created Node class for Huffman-tree

---
+++ src/main/java/com/github/dozedoff/commonj/encoding/Node.java
@@ -0,0 +1,45 @@
+/*  Copyright (C) 2013  Nicholas Wright
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.dozedoff.commonj.encoding;
+
+public class Node {
+	Node leftChild;
+	Node rightChild;
+	Node parent;
+	
+	double probability = -1.0;
+	char symbol;
+	
+	boolean coding;
+
+	public Node(Node leftChild, Node rightChild, Node parent) {
+		this.leftChild = leftChild;
+		this.rightChild = rightChild;
+		this.parent = parent;
+	}
+	
+	public boolean isLeaf() {
+		return (leftChild == null && rightChild == null);
+	}
+	
+	public boolean isRoot() {
+		return parent == null;
+	}
+	
+	public boolean isJunction() {
+		return ((! isLeaf()) && (! isRoot()));
+	}
+}
