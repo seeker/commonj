2e2cbc05719ad2c2c31ac02db74fa0e8ce177c0c
dozedoff <dozedoffagain@gmail.com>
2013-05-07 19:06:08 +0200

    Added ignore statement to un-implemented test

--- src/test/java/com/github/dozedoff/commonj/encoding/HuffmanTest.java
+++ src/test/java/com/github/dozedoff/commonj/encoding/HuffmanTest.java
@@ -17,10 +17,12 @@ package com.github.dozedoff.commonj.encoding;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class HuffmanTest {
 
+	@Ignore
 	@Test
 	public void test() {
 		fail("Not yet implemented");
