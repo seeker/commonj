fdccf6c40584cedbf5a013f05c6f169efb262e9d
dozedoff <dozedoffagain@gmail.com>
2013-05-10 02:53:29 +0200

    Created class for object pairs

---
+++ src/main/java/com/github/dozedoff/commonj/util/Pair.java
@@ -0,0 +1,42 @@
+/*  Copyright (C) 2012  Nicholas Wright
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.dozedoff.commonj.util;
+
+public class Pair<V, T> {
+	private V left;
+	private T right;
+	
+	public Pair(V left, T right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	public V getLeft() {
+		return left;
+	}
+
+	public void setLeft(V left) {
+		this.left = left;
+	}
+
+	public T getRight() {
+		return right;
+	}
+
+	public void setRight(T right) {
+		this.right = right;
+	}
+}
