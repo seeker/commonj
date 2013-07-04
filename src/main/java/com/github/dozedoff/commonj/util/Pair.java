31284d782a7d3292a0d047ea276765975c6d7f29
dozedoff <dozedoffagain@gmail.com>
2013-07-04 23:07:31 +0200

    Added overrode equals method for Pair

--- src/main/java/com/github/dozedoff/commonj/util/Pair.java
+++ src/main/java/com/github/dozedoff/commonj/util/Pair.java
@@ -18,7 +18,7 @@ package com.github.dozedoff.commonj.util;
 public class Pair<V, T> {
 	private V left;
 	private T right;
-	
+
 	public Pair(V left, T right) {
 		this.left = left;
 		this.right = right;
@@ -38,5 +38,25 @@ public class Pair<V, T> {
 
 	public void setRight(T right) {
 		this.right = right;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Pair)) {
+			return false;
+		}
+
+		Pair pair = (Pair) obj;
+
+		if (!pair.getLeft().equals(getLeft())) {
+			return false;
+		}
+
+		if (!pair.getRight().equals(getRight())) {
+			return false;
+		}
+
+		return true;
 	}
 }
