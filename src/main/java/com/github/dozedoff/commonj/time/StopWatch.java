75be441ab6d1289f0c0b858930f1fd0f7314aa15
dozedoff <dozedoffagain@gmail.com>
2013-07-05 11:17:11 +0200

    Fixed wrong time when reading running stopwatch

--- src/main/java/com/github/dozedoff/commonj/time/StopWatch.java
+++ src/main/java/com/github/dozedoff/commonj/time/StopWatch.java
@@ -21,64 +21,75 @@ public class StopWatch {
 	final String format = "%1$02d:%2$02d:%3$02d.%4$03d";
 	boolean isRunning = false;
 	long startTime, stopTime;
-	
+
 	final long CONST_H = 3600000;
 	final long CONST_M = 60000;
 	final long CONST_S = 1000;
-	
-	public boolean start(){
-		startTime = Calendar.getInstance().getTimeInMillis();
-		isRunning = true;
+
+	public boolean start() {
+		if (!isRunning) {
+			startTime = getCurrentTime();
+			isRunning = true;
+		}
+
 		return isRunning;
 	}
-	
-	public boolean stop(){
-		stopTime = Calendar.getInstance().getTimeInMillis();
+
+	public boolean stop() {
+		stopTime = getCurrentTime();
 		isRunning = false;
 		return isRunning;
 	}
-	
-	public String getTime(){
+
+	public String getTime() {
 		return convertTime(getTimeMilli());
 	}
-	
-	public long getTimeMilli(){
-		return stopTime-startTime;
+
+	public long getTimeMilli() {
+		if (isRunning) {
+			return getCurrentTime() - startTime;
+		} else {
+			return stopTime - startTime;
+		}
 	}
-	
-	public boolean isRunning(){
+
+	public boolean isRunning() {
 		return isRunning;
 	}
-	
-	public void reset(){
+
+	public void reset() {
 		isRunning = false;
 		startTime = 0;
 		stopTime = 0;
 	}
-	
-	protected String convertTime(long time){
-		
-		if(time == 0){
+
+	private long getCurrentTime() {
+		return Calendar.getInstance().getTimeInMillis();
+	}
+
+	protected String convertTime(long time) {
+
+		if (time == 0) {
 			return "00:00:00.000";
 		}
-		
-		if(time < 0){
+
+		if (time < 0) {
 			return "--:--:--.---";
 		}
-		
+
 		long remainder, hours, minutes, seconds, milliSec;
-		
+
 		hours = time / CONST_H;
-		remainder = time - (hours*CONST_H);
-		
+		remainder = time - (hours * CONST_H);
+
 		minutes = remainder / CONST_M;
-		remainder = remainder - (minutes*CONST_M);
-		
+		remainder = remainder - (minutes * CONST_M);
+
 		seconds = remainder / CONST_S;
-		remainder = remainder - (seconds*CONST_S);
-		
+		remainder = remainder - (seconds * CONST_S);
+
 		milliSec = remainder;
-		
-		return String.format(format, hours,minutes,seconds,milliSec);
+
+		return String.format(format, hours, minutes, seconds, milliSec);
 	}
 }
