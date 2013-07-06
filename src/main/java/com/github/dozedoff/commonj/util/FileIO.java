d46f38e0fdd45308d8fce4b76436da000c522591
dozedoff <dozedoffagain@gmail.com>
2013-07-06 18:57:24 +0200

    Fixed year in comment

--- src/main/java/com/github/dozedoff/commonj/util/FileIO.java
+++ src/main/java/com/github/dozedoff/commonj/util/FileIO.java
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2012  Nicholas Wright
+/*  Copyright (C) 2013  Nicholas Wright
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
