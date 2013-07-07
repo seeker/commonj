ca390ce8f9fa114ce688efa15e1ef32e89c291c7
dozedoff <dozedoffagain@gmail.com>
2013-07-07 22:05:06 +0200

    Added tests for FileFilter

--- src/test/java/com/github/dozedoff/commonj/filefilter/FileFilterTest.java
+++ src/test/java/com/github/dozedoff/commonj/filefilter/FileFilterTest.java
@@ -15,15 +15,44 @@
  */
 package com.github.dozedoff.commonj.filefilter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class FileFilterTest {
+	FileFilter ff;
 
-	@Test
-	public void testAccept() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		ff = new FileFilter();
 	}
 
+	@Test
+	public void testFile() throws IOException {
+		File file = Files.createTempFile("test", ".dat").toFile();
+		assertTrue(ff.accept(file));
+	}
+
+	@Test
+	public void testDirectory() throws IOException {
+		File directory = Files.createTempDirectory("test").toFile();
+		assertFalse(ff.accept(directory));
+	}
+
+	@Test
+	public void testNonExistantFile() {
+		File file = new File("test");
+		assertFalse(ff.accept(file));
+	}
+
+	@Test
+	public void testNull() {
+		assertFalse(ff.accept(null));
+	}
 }
