d5c68f057b0cf0668f3d44f3c9235f55d11746ee
dozedoff <dozedoffagain@gmail.com>
2013-07-07 10:43:51 +0200

    Added tests for DirectoryFilter

--- src/test/java/com/github/dozedoff/commonj/filefilter/DirectoryFilterTest.java
+++ src/test/java/com/github/dozedoff/commonj/filefilter/DirectoryFilterTest.java
@@ -15,15 +15,45 @@
  */
 package com.github.dozedoff.commonj.filefilter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class DirectoryFilterTest {
+	DirectoryFilter df;
 
-	@Test
-	public void testAccept() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		df = new DirectoryFilter();
 	}
 
+	@Test
+	public void testNotDirectory() throws IOException {
+		File file = Files.createTempFile("test", ".dat").toFile();
+		assertFalse(df.accept(file));
+	}
+
+	@Test
+	public void testDirectory() throws IOException {
+		File directory = Files.createTempDirectory("test").toFile();
+		assertTrue(df.accept(directory));
+	}
+
+	@Test
+	public void testInaccessibleDirectory() {
+		File directory = new File(".");
+		assertTrue(df.accept(directory));
+	}
+
+	@Test
+	public void testNonExistantFile() {
+		File directory = new File("test");
+		assertFalse(df.accept(directory));
+	}
 }
