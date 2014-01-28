9e83b62e83b4f0e75b4706e32a50acab042f84ce
dozedoff <dozedoffagain@gmail.com>
2014-01-28 19:27:26 +0100

    Replace URL variables with ones generated from Enums

--- src/test/java/com/github/dozedoff/commonj/net/GetBinaryTest.java
+++ src/test/java/com/github/dozedoff/commonj/net/GetBinaryTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -31,11 +33,14 @@ public class GetBinaryTest {
 
 	static final int SERVER_PORT = 5400;
 
+	private static enum Pages {
+		data, data2, range, wait
+	};
+
+	HashMap<Pages, URL> pageURLs = new HashMap<>();
+
 	static final int READ_TIMEOUT_CLASS = 100;
 	static final int READ_TIMEOUT_TEST = 1000;
-
-	String url = "http://localhost:" + SERVER_PORT + "/", url2 = "http://localhost:" + SERVER_PORT + "/2", urlWait = "http://localhost:"
-			+ SERVER_PORT + "/wait";
 
 	@BeforeClass
 	public static void startServer() throws Exception {
@@ -58,27 +63,27 @@ public class GetBinaryTest {
 	@Test
 	public void testGetViaHttp() throws IOException {
 		testData = generateRandomData(25);
-		byte[] data = getBinary.getViaHttp(url);
+		byte[] data = getBinary.getViaHttp(getURL(Pages.data));
 		assertThat(data, is(testData));
 	}
 
 	@Test(timeout = READ_TIMEOUT_TEST, expected = SocketTimeoutException.class)
 	public void testGetViaHttpTimeout() throws IOException {
 		getBinary.setReadTimeout(READ_TIMEOUT_CLASS);
-		getBinary.getViaHttp(urlWait);
+		getBinary.getViaHttp(getURL(Pages.wait));
 	}
 
 	@Test
 	public void testGetLenght() throws Exception {
 		testData = generateRandomData(25);
-		Long size = getBinary.getLenght(new URL(url));
+		Long size = getBinary.getLenght(getURL(Pages.data));
 		assertThat(size, is((long) testData.length));
 	}
 
 	@Test(timeout = READ_TIMEOUT_TEST, expected = SocketTimeoutException.class)
 	public void testGetLenghtTimeOut() throws Exception {
 		getBinary.setReadTimeout(READ_TIMEOUT_CLASS);
-		getBinary.getLenght(new URL(urlWait));
+		getBinary.getLenght(getURL(Pages.wait));
 	}
 
 	@Test
@@ -92,35 +97,35 @@ public class GetBinaryTest {
 		testData = generateRandomData(25);
 		byte[] subSet = Arrays.copyOfRange(testData, 10, 25);
 
-		byte[] data = getBinary.getRange(new URL(url), 10, 15);
+		byte[] data = getBinary.getRange(getURL(Pages.data), 10, 15);
 		assertThat(data, is(subSet));
 	}
 
 	@Test(timeout = READ_TIMEOUT_TEST, expected = SocketTimeoutException.class)
 	public void testGetRangeTimeout() throws Exception {
 		getBinary.setReadTimeout(READ_TIMEOUT_CLASS);
-		getBinary.getRange(new URL(urlWait), 10, 15);
+		getBinary.getRange(getURL(Pages.wait), 10, 15);
 	}
 
 	@Test(timeout = 5000)
 	public void testReUse() throws Exception {
-		assertThat(getBinary.getViaHttp(url), is(testData));
-		assertThat(getBinary.getViaHttp(url2), is(testData2));
-		assertThat(getBinary.getViaHttp(url), is(testData));
+		assertThat(getBinary.getViaHttp(getURL(Pages.data)), is(testData));
+		assertThat(getBinary.getViaHttp(getURL(Pages.data2)), is(testData2));
+		assertThat(getBinary.getViaHttp(getURL(Pages.data)), is(testData));
 	}
 
 	@Test(timeout = READ_TIMEOUT_TEST, expected = SocketTimeoutException.class)
 	public void testConnectionTimeout() throws Exception {
 		getBinary.setMaxRetry(0);
 		getBinary.setReadTimeout(READ_TIMEOUT_CLASS);
-		getBinary.getViaHttp(urlWait);
+		getBinary.getViaHttp(getURL(Pages.wait));
 	}
 
 	@Test(timeout = 10000, expected = SocketException.class)
 	public void testConnectionFail() throws Exception {
 		getBinary.setMaxRetry(0);
 		server.stop();
-		getBinary.getViaHttp(url);
+		getBinary.getViaHttp(getURL(Pages.data));
 	}
 
 	static class TestHandler extends AbstractHandler {
@@ -132,10 +137,10 @@ public class GetBinaryTest {
 			response.setStatus(HttpServletResponse.SC_OK);
 			baseRequest.setHandled(true);
 
-			if (request.getRequestURI().equals("/2")) {
+			if (matchesPage(request, Pages.data2)) {
 				response.getOutputStream().write(testData2);
 				response.getOutputStream().close();
-			} else if (request.getRequestURI().equals("/wait")) {
+			} else if (matchesPage(request, Pages.wait)) {
 				try {
 					Thread.sleep(12000);
 				} catch (InterruptedException e) {
@@ -153,9 +158,11 @@ public class GetBinaryTest {
 				byte[] selection = Arrays.copyOfRange(testData, start, offset);
 				response.getOutputStream().write(selection);
 				response.getOutputStream().close();
-			} else {
+			} else if (matchesPage(request, Pages.data)) {
 				response.getOutputStream().write(testData);
 				response.getOutputStream().close();
+			} else {
+				throw new IllegalArgumentException("Unknown page");
 			}
 		}
 	}
@@ -167,5 +174,28 @@ public class GetBinaryTest {
 		}
 
 		return randomData;
+	}
+
+	private static boolean matchesPage(HttpServletRequest request, Pages page) {
+		return (request.getRequestURI().equals("/" + page.toString()));
+	}
+
+	private URL getURL(Pages page) throws MalformedURLException {
+		if (pageURLs.containsKey(page)) {
+			return pageURLs.get(page);
+		} else {
+			URL pageUrl = createURL(page);
+			pageURLs.put(page, pageUrl);
+			return pageUrl;
+		}
+	}
+
+	private URL createURL(Pages page) throws MalformedURLException {
+		String pageName = page.toString();
+		String constructedUrl = "http://localhost:" + SERVER_PORT + "/" + pageName;
+
+		URL pageUrl = new URL(constructedUrl);
+
+		return pageUrl;
 	}
 }
