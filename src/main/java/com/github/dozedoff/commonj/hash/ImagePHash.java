77efb253b456ca24c647a0957f48c5cda63d1856
dozedoff <dozedoffagain@gmail.com>
2014-02-02 20:15:28 +0100

    Eclipse auto format

--- src/main/java/com/github/dozedoff/commonj/hash/ImagePHash.java
+++ src/main/java/com/github/dozedoff/commonj/hash/ImagePHash.java
@@ -72,8 +72,7 @@ public class ImagePHash {
 	 * 
 	 * @param is
 	 *            file to hash
-	 * @return a 'binary string' (like. 001010111011100010) which is easy to do
-	 *         a hamming distance on.
+	 * @return a 'binary string' (like. 001010111011100010) which is easy to do a hamming distance on.
 	 * @throws IOException
 	 */
 	public String getHash(InputStream is) throws IOException {
@@ -109,25 +108,21 @@ public class ImagePHash {
 	public double[][] calculateDctMap(BufferedImage img) throws IOException {
 
 		/*
-		 * 1. Reduce size. Like Average Hash, pHash starts with a small image.
-		 * However, the image is larger than 8x8; 32x32 is a good size. This is
-		 * really done to simplify the DCT computation and not because it is
-		 * needed to reduce the high frequencies.
+		 * 1. Reduce size. Like Average Hash, pHash starts with a small image. However, the image is larger than 8x8; 32x32 is a good size.
+		 * This is really done to simplify the DCT computation and not because it is needed to reduce the high frequencies.
 		 */
 		img = resize(img, size, size);
 
 		/*
-		 * 2. Reduce color. The image is reduced to a grayscale just to further
-		 * simplify the number of computations.
+		 * 2. Reduce color. The image is reduced to a grayscale just to further simplify the number of computations.
 		 */
 		img = grayscale(img);
 
 		double[][] vals = reduceColor(img);
 
 		/*
-		 * 3. Compute the DCT. The DCT separates the image into a collection of
-		 * frequencies and scalars. While JPEG uses an 8x8 DCT, this algorithm
-		 * uses a 32x32 DCT.
+		 * 3. Compute the DCT. The DCT separates the image into a collection of frequencies and scalars. While JPEG uses an 8x8 DCT, this
+		 * algorithm uses a 32x32 DCT.
 		 */
 		// long start = System.currentTimeMillis();
 		double[][] dctVals = applyDCT(vals);
@@ -139,13 +134,10 @@ public class ImagePHash {
 
 	private String convertToBitString(double[][] dctVals, double avg) {
 		/*
-		 * 6. Further reduce the DCT. This is the magic step. Set the 64 hash
-		 * bits to 0 or 1 depending on whether each of the 64 DCT values is
-		 * above or below the average value. The result doesn't tell us the
-		 * actual low frequencies; it just tells us the very-rough relative
-		 * scale of the frequencies to the mean. The result will not vary as
-		 * long as the overall structure of the image remains the same; this can
-		 * survive gamma and color histogram adjustments without a problem.
+		 * 6. Further reduce the DCT. This is the magic step. Set the 64 hash bits to 0 or 1 depending on whether each of the 64 DCT values
+		 * is above or below the average value. The result doesn't tell us the actual low frequencies; it just tells us the very-rough
+		 * relative scale of the frequencies to the mean. The result will not vary as long as the overall structure of the image remains the
+		 * same; this can survive gamma and color histogram adjustments without a problem.
 		 */
 
 		String hash = "";
@@ -180,15 +172,13 @@ public class ImagePHash {
 
 	private double calcDctAverage(double[][] dctVals) {
 		/*
-		 * 4. Reduce the DCT. This is the magic step. While the DCT is 32x32,
-		 * just keep the top-left 8x8. Those represent the lowest frequencies in
-		 * the picture.
+		 * 4. Reduce the DCT. This is the magic step. While the DCT is 32x32, just keep the top-left 8x8. Those represent the lowest
+		 * frequencies in the picture.
 		 */
 		/*
-		 * 5. Compute the average value. Like the Average Hash, compute the mean
-		 * DCT value (using only the 8x8 DCT low-frequency values and excluding
-		 * the first term since the DC coefficient can be significantly
-		 * different from the other values and will throw off the average).
+		 * 5. Compute the average value. Like the Average Hash, compute the mean DCT value (using only the 8x8 DCT low-frequency values and
+		 * excluding the first term since the DC coefficient can be significantly different from the other values and will throw off the
+		 * average).
 		 */
 		double total = 0;
 
