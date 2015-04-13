7a3928651588687f3f15118bb65ba874d299177b
dozedoff <dozedoffagain@gmail.com>
2015-04-13 15:15:05 +0200

    REFACTOR replaced custom String hamming code with Long.parseUnsignedLong

--- src/main/java/com/github/dozedoff/commonj/util/Bits.java
+++ src/main/java/com/github/dozedoff/commonj/util/Bits.java
@@ -2,14 +2,7 @@ package com.github.dozedoff.commonj.util;
 
 public class Bits {
 	public static int hammingDistance(String left, String right) {
-		int counter = 0;
-		for (int k = 0; k < left.length(); k++) {
-			if (left.charAt(k) != right.charAt(k)) {
-				counter++;
-			}
-		}
-
-		return counter;
+		return hammingDistance(Long.parseUnsignedLong(left, 2), Long.parseUnsignedLong(right, 2));
 	}
 
 	public static int hammingDistance(long left, long right) {
