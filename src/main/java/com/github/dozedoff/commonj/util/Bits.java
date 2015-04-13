672f5ebd1a39e2f2e0ea2c3846c3a3b8313d2095
dozedoff <dozedoffagain@gmail.com>
2015-04-13 19:02:16 +0200

    Removed API 1.8 only code

--- src/main/java/com/github/dozedoff/commonj/util/Bits.java
+++ src/main/java/com/github/dozedoff/commonj/util/Bits.java
@@ -2,7 +2,7 @@ package com.github.dozedoff.commonj.util;
 
 public class Bits {
 	public static int hammingDistance(String left, String right) {
-		return hammingDistance(Long.parseUnsignedLong(left, 2), Long.parseUnsignedLong(right, 2));
+		return hammingDistance(Long.parseLong(left, 2), Long.parseLong(right, 2));
 	}
 
 	public static int hammingDistance(long left, long right) {
