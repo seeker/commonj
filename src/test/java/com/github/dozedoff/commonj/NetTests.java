1a8c1d6fa3a7e10d7f1e627c0587adcf9f0088a6
dozedoff <dozedoffagain@gmail.com>
2015-04-21 21:43:06 +0200

    Tests for DownloadItem

--- src/test/java/com/github/dozedoff/commonj/NetTests.java
+++ src/test/java/com/github/dozedoff/commonj/NetTests.java
@@ -9,13 +9,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.github.dozedoff.commonj.net.DownloadItemTest;
 import com.github.dozedoff.commonj.net.GetBinaryTest;
 import com.github.dozedoff.commonj.net.GetHtmlTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
 	GetBinaryTest.class, 
-	GetHtmlTest.class
+	GetHtmlTest.class,
+	DownloadItemTest.class
 })
 public class NetTests {
 
