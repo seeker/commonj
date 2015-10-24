3d681cf11c726fe059964ccc34ed4603f86f0f08
Nicholas Wright <dozedoffagain@gmail.com>
2015-10-24 19:27:19 +0200

    Rewrote tests, removed Jetty http tests

--- src/test/java/com/github/dozedoff/commonj/net/GetBinaryTest.java
+++ src/test/java/com/github/dozedoff/commonj/net/GetBinaryTest.java
@@ -1,284 +1,124 @@
-/* The MIT License (MIT)
- * Copyright (c) 2014 Nicholas Wright
- * http://opensource.org/licenses/MIT
- */
-
 package com.github.dozedoff.commonj.net;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Response;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GetBinaryTest {
-	static final int SERVER_PORT = 5400;
-	static final int TEST_DATA_SIZE = 25;
-	static final int READ_TIMEOUT_CLASS = 100;
-	static final int READ_TIMEOUT_TEST = 1500;
+	private IHttpClient httpClient;
+	private GetBinary cut;
+	private URL testUrl;
 	
-	private static enum Pages {
-		data, range, wait, notok, error
-	};
-	
-	GetBinary getBinary;
-	static Server server;
-	static TestHandler testHandler;
-	HashMap<Pages, URL> pageURLs = new HashMap<>();
-
-	@BeforeClass
-	public static void startServer() throws Exception {
-		server = new Server(SERVER_PORT);
-		testHandler = new TestHandler();
-		server.setHandler(testHandler);
-		server.start();
-	}
-
-	@AfterClass
-	public static void stopServer() throws Exception {
-		server.stop();
-	}
+	private static final int OFFSET = 0;
+	private static final long RANGE = 100;
+	private static final String TEST_STRING = "http://example.com";
 
 	@Before
 	public void setUp() throws Exception {
-		JettyHttpClient httpClient = new JettyHttpClient();
-		getBinary = new GetBinary(httpClient);
-		testHandler.setTestData(new byte[0]);
-		server.start();
-	}
-
-	@Test
-	public void testInvalidTimeoutSetting() {
-		assertThat(getBinary.setReadTimeout(-100), is(false));
-	}
-
-	@Test
-	public void testGetViaHttp() throws IOException {
-		testHandler.setTestData(generateRandomData(TEST_DATA_SIZE));
-		byte[] data = getBinary.getViaHttp(getURL(Pages.data));
-		assertThat(data, is(testHandler.getTestData()));
-	}
-	
-	@Test
-	public void testGetViaHttpString() throws IOException {
-		testHandler.setTestData(generateRandomData(TEST_DATA_SIZE));
-		byte[] data = getBinary.getViaHttp(getURL(Pages.data).toString());
-		assertThat(data, is(testHandler.getTestData()));
-	}
-
-	@Test(timeout = READ_TIMEOUT_TEST, expected = SocketTimeoutException.class)
-	public void testGetViaHttpTimeout() throws IOException {
-		getBinary.setReadTimeout(READ_TIMEOUT_CLASS);
-		getBinary.getViaHttp(getURL(Pages.wait));
-	}
-	
-	@Test(expected=PageLoadException.class)
-	public void testGetViaHttpBadRequest() throws IOException {
-		getBinary.getViaHttp(getURL(Pages.notok));
-	}
-	
-	@Test(expected=IOException.class)
-	public void testGetViaHttpIOExceptiom() throws IOException {
-		getBinary.getViaHttp(getURL(Pages.error));
+		httpClient = mock(IHttpClient.class);
+		cut = new GetBinary(httpClient);
+		testUrl = new URL(TEST_STRING);
 	}
 
 	@Test
 	public void testGetLenght() throws Exception {
-		testHandler.setTestData(generateRandomData(TEST_DATA_SIZE));
-		Long size = getBinary.getLenght(getURL(Pages.data));
-		assertThat(size, is((long) testHandler.getTestData().length));
+		cut.getLenght(testUrl);
+		verify(httpClient).getLenght(testUrl);
 	}
-
-	@Test(timeout = READ_TIMEOUT_TEST, expected = SocketTimeoutException.class)
-	public void testGetLenghtTimeOut() throws Exception {
-		getBinary.setReadTimeout(READ_TIMEOUT_CLASS);
-		getBinary.getLenght(getURL(Pages.wait));
+	
+	@Test(expected=SocketTimeoutException.class)
+	public void testGetLenghtTimeout() throws Exception {
+		when(httpClient.getLenght(any(URL.class))).thenThrow(new TimeoutException());
+		cut.getLenght(testUrl);
+	}
+	
+	@Test
+	public void testGetLenghtInterrupted() throws Exception {
+		when(httpClient.getLenght(any(URL.class))).thenThrow(new InterruptedException());
+		assertThat(cut.getLenght(testUrl), is(-1L));
 	}
 
 	@Test
 	public void testGetHeader() throws Exception {
-		int dataLength = 5000;
-		testHandler.setTestData(generateRandomData(dataLength));
-		Map<String, List<String>> header = getBinary.getHeader(getURL(Pages.data));
-
-		assertThat(header.containsKey("Content-Length"), is(true));
-		assertThat(header.get("Content-Length").get(0), is(Integer.toString(dataLength)));
+		cut.getHeader(testUrl);
+		verify(httpClient).getHeader(testUrl);
 	}
 
 	@Test
+	public void testGetHeaderInterrupted() throws Exception {
+		when(cut.getHeader(testUrl)).thenThrow(new InterruptedException());
+		Map<String,List<String>> result = cut.getHeader(testUrl);
+
+		assertThat(result.isEmpty(), is(true));
+	}
+	
+	@Test
 	public void testGetRange() throws Exception {
-		int dataOffset = 10;
-		testHandler.setTestData(generateRandomData(TEST_DATA_SIZE));
-		byte[] subSet = Arrays.copyOfRange(testHandler.getTestData(), dataOffset, TEST_DATA_SIZE);
-
-		byte[] data = getBinary.getRange(getURL(Pages.data), dataOffset, TEST_DATA_SIZE-dataOffset);
-		assertThat(data, is(subSet));
+		cut.getRange(testUrl, OFFSET, RANGE);
+		
+		verify(httpClient).getDataRange(testUrl, OFFSET, RANGE);
 	}
-
-	@Test(timeout = READ_TIMEOUT_TEST, expected = SocketTimeoutException.class)
+	
+	@Test(expected=SocketTimeoutException.class)
 	public void testGetRangeTimeout() throws Exception {
-		int dataOffset = 10;
-		getBinary.setReadTimeout(READ_TIMEOUT_CLASS);
-		getBinary.getRange(getURL(Pages.wait), dataOffset, TEST_DATA_SIZE-dataOffset);
+		when(httpClient.getDataRange(testUrl, OFFSET, RANGE)).thenThrow(new TimeoutException());
+		cut.getRange(testUrl, OFFSET, RANGE);
+	}
+	
+	@Test(expected=PageLoadException.class)
+	public void testGetRangeInterrupted() throws Exception {
+		when(httpClient.getDataRange(testUrl, OFFSET, RANGE)).thenThrow(new InterruptedException());
+		cut.getRange(testUrl, OFFSET, RANGE);
 	}
 
-	@Test(timeout = 5000)
-	public void testReUse() throws Exception {
-		int iterations = 3;
+	@Test
+	public void testGetViaHttpString() throws Exception {
+		cut.getViaHttp(TEST_STRING);
 		
-		for (int i=0; i<iterations; i++){
-			testHandler.setTestData(generateRandomData(TEST_DATA_SIZE));
-			assertThat(getBinary.getViaHttp(getURL(Pages.data)), is(testHandler.getTestData()));
-		}
+		verify(httpClient).getData(new URL(TEST_STRING));
 	}
 
-	@Test(timeout = READ_TIMEOUT_TEST, expected = SocketTimeoutException.class)
-	public void testConnectionTimeout() throws Exception {
-		getBinary.setMaxRetry(0);
-		getBinary.setReadTimeout(READ_TIMEOUT_CLASS);
-		getBinary.getViaHttp(getURL(Pages.wait));
-	}
-
-	static class TestHandler extends AbstractHandler {
-		private byte[] testData;
+	@Test
+	public void testGetViaHttpURL() throws Exception {
+		cut.getViaHttp(testUrl);
 		
-		public byte[] getTestData() {
-			return testData;
-		}
-
-		public void setTestData(byte[] testData) {
-			this.testData = testData;
-		}
-
-		@Override
-		public void handle(String arg0, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException,
-				ServletException {
-
-			response.setContentType("application/octet-stream");
-			response.setStatus(HttpServletResponse.SC_OK);
-			baseRequest.setHandled(true);
-
-			String pageName = extractPage(request);
-
-			Pages selectedPage = null;
-
-			try {
-				selectedPage = Pages.valueOf(Pages.class, pageName);
-			} catch (IllegalArgumentException iae) {
-				fail(pageName + " is not a valid page Enum");
-			} catch (NullPointerException npe) {
-				fail("Page name was null");
-			}
-
-			switch (selectedPage) {
-			case data:
-				processDataRequest(request, response, this.testData);
-				break;
-
-			case wait:
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-				}
-				break;
-				
-			case notok:
-				response.setStatus(Response.SC_BAD_REQUEST);
-				break;
-				
-			case error:
-				response.sendError(Response.SC_BAD_REQUEST);
-				break;
-				
-			default:
-				throw new IllegalArgumentException("Unknown page");
-			}
-		}
+		verify(httpClient).getData(testUrl);
+	}
+	
+	@Test
+	public void testSetReadTimeoutPositive() throws Exception {
+		assertThat(cut.setReadTimeout(42), is(true));
+	}
+	
+	@Test
+	public void testSetReadTimeoutZero() throws Exception {
+		assertThat(cut.setReadTimeout(0), is(true));
+	}
+	
+	@Test
+	public void testSetReadTimeoutNegative() throws Exception {
+		assertThat(cut.setReadTimeout(-42), is(false));
 	}
 
-	private static boolean isRangeRequest(HttpServletRequest request) {
-		return (request.getHeader("Range") != null && request.getHeader("Range").contains("bytes"));
-	}
-
-	private static void processRangeRequest(HttpServletRequest request, HttpServletResponse response, byte[] data) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		sb.append(request.getHeader("Range"));
-
-		sb.replace(0, 6, "");
-		String[] marker = sb.toString().split("-");
-
-		int start = Integer.parseInt(marker[0]);
-		int offset = start + Integer.parseInt(marker[1]);
-
-		byte[] selection = Arrays.copyOfRange(data, start, offset);
-		response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-		response.getOutputStream().write(selection);
-		response.getOutputStream().close();
-	}
-
-	private static void processDataRequest(HttpServletRequest request, HttpServletResponse response, byte[] data) throws IOException {
-		if (isRangeRequest(request)) {
-			processRangeRequest(request, response, data);
-		} else {
-			response.getOutputStream().write(data);
-			response.getOutputStream().close();
-		}
-	}
-
-	private byte[] generateRandomData(int numOfBytes) {
-		byte[] randomData = new byte[numOfBytes];
-		for (int i = 0; i < numOfBytes; i++) {
-			randomData[i] = (byte) (Math.random() * Byte.MAX_VALUE);
-		}
-
-		return randomData;
-	}
-
-	private static String extractPage(HttpServletRequest request) {
-		String requestUri = request.getRequestURI();
-		int pageIndexStart = requestUri.lastIndexOf("/");
-
-		String pageName = requestUri.substring(pageIndexStart + 1);
-
-		return pageName;
-	}
-
-	private URL getURL(Pages page) throws MalformedURLException {
-		if (pageURLs.containsKey(page)) {
-			return pageURLs.get(page);
-		} else {
-			URL pageUrl = createURL(page);
-			pageURLs.put(page, pageUrl);
-			return pageUrl;
-		}
-	}
-
-	private URL createURL(Pages page) throws MalformedURLException {
-		String pageName = page.toString();
-		String constructedUrl = "http://localhost:" + SERVER_PORT + "/" + pageName;
-
-		URL pageUrl = new URL(constructedUrl);
-
-		return pageUrl;
+	@Test
+	public void testDownload() throws Exception {
+		cut.download(testUrl);
+		
+		verify(httpClient).getData(testUrl);
 	}
 }
