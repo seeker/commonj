60832dcaf846528772ce6fd031c468303894200b
Nicholas Wright <dozedoffagain@gmail.com>
2015-11-07 17:38:45 +0100

    More findbugs fixes

--- src/main/java/com/github/dozedoff/commonj/settings/AbstractSettingsValidator.java
+++ src/main/java/com/github/dozedoff/commonj/settings/AbstractSettingsValidator.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class AbstractSettingsValidator implements ISettingsValidator {
-	protected static final Logger logger = LoggerFactory.getLogger(AbstractSettings.class);
+	protected static final Logger logger = LoggerFactory.getLogger(AbstractSettingsValidator.class);
 	private boolean allOk = true;
 
 	protected void checkGreaterZero(Object param, int value) {
@@ -22,7 +22,7 @@ public abstract class AbstractSettingsValidator implements ISettingsValidator {
 	protected void checkRange(Object param, int value, int min, int max) {
 		if (!((value >= min) && (value <= max))) {
 			setOk(false);
-			Object loggerData[] = { param.toString(), };
+			Object loggerData[] = { param.toString(), min, max, value };
 			logger.warn("Value for {} must be between {} and {}, currently set to {}", loggerData);
 		}
 	}
