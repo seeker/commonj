fefea157b11b2da14ceda08bc2a1cc6f52b01d66
Nicholas Wright <dozedoffagain@gmail.com>
2016-02-01 14:07:24 +0100

    Added OpenCV for Image processing

--- src/main/java/com/github/dozedoff/commonj/util/ImageUtil.java
+++ src/main/java/com/github/dozedoff/commonj/util/ImageUtil.java
@@ -1,16 +1,41 @@
 package com.github.dozedoff.commonj.util;
 
+import static org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static org.bytedeco.javacpp.opencv_imgproc.resize;
+import static org.bytedeco.javacpp.opencv_core.CV_8UC3;
+import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
+
 import java.awt.Graphics2D;
 import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorConvertOp;
+import java.awt.image.DataBufferByte;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.file.Path;
 
+import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 
+import org.bytedeco.javacpp.indexer.ByteBufferIndexer;
+import org.bytedeco.javacpp.indexer.UByteBufferIndexer;
+import org.bytedeco.javacpp.indexer.UByteIndexer;
+import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.opencv_core.CvType;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Size;
+import org.bytedeco.javacpp.helper.opencv_core;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameConverter;
+import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter.ToMat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.jcraft.jsch.Buffer;
 
 public class ImageUtil {
 	private static final ColorConvertOp colorConverter = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
@@ -75,5 +100,45 @@ public class ImageUtil {
 
 	public static BufferedImage readImage(InputStream is) throws IOException {
 		return ImageIO.read(is);
+	}
+	
+	public static Mat openCvLoadGrayscale(Path file) throws IIOException {
+		Mat image = imread(file.toString(), CV_LOAD_IMAGE_GRAYSCALE);
+
+		if (image.empty()) {
+			throw new IIOException("Image " + file + " is empty");
+		}
+
+		return image;
+	}
+
+	public static Mat openCvResizeImage(Mat image, int width, int height) {
+		Size sz = new Size(width, height);
+		Mat resized = new Mat(sz);
+		resize(image, resized, sz);
+
+		return resized;
+	}
+
+	public static double[][] openCvToMatrix(Mat image) {
+		int rows = image.rows();
+		int cols = image.cols();
+		double matrix[][] = new double[rows][cols];
+
+		UByteBufferIndexer idx = image.createIndexer();
+
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+				matrix[row][col] = idx.get(row, col);
+			}
+		}
+
+		return matrix;
+	}
+
+	public static Mat openCvConvertToGrayscale(Mat image) {
+		Mat gray = new Mat();
+		cvtColor(image, gray, org.bytedeco.javacpp.opencv_core.CV_8UC1);
+		return gray;
 	}
 }
