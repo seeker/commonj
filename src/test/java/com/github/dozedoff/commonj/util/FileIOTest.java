e8ab9248effb192360c7bb9cec5af9af684847e8
Nicholas Wright <dozedoffagain@gmail.com>
2017-09-22 11:15:47 +0200

    Removed deprecated code from FileIO

--- src/test/java/com/github/dozedoff/commonj/util/FileIOTest.java
+++ src/test/java/com/github/dozedoff/commonj/util/FileIOTest.java
@@ -5,16 +5,13 @@
  */
 package com.github.dozedoff.commonj.util;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
 import java.nio.file.Files;
 
 import org.junit.Before;
@@ -24,10 +21,6 @@ import org.mockito.Mockito;
 
 import com.github.dozedoff.commonj.file.BinaryFileWriter;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
-@SuppressFBWarnings("FCBL_FIELD_COULD_BE_LOCAL")
-@SuppressWarnings("deprecation")
 public class FileIOTest {
 	private static final int SAMPLE_SIZE = 10;
 	private byte[] randomData;
@@ -47,45 +40,6 @@ public class FileIOTest {
 	}
 
 	@Test
-	public void testOpenAsFileInputStream() throws IOException {
-		FileInputStream fis = FileIO.openAsFileInputStream(testFile);
-		byte[] data = new byte[SAMPLE_SIZE];
-		int read = fis.read(data, 0, SAMPLE_SIZE);
-		fis.close();
-
-		assertThat(read, is(SAMPLE_SIZE));
-		assertArrayEquals(randomData, data);
-	}
-
-	@Test
-	public void testOpenAsFileInputStreamInvalidFile() throws IOException {
-		File file = new File("foo");
-		FileInputStream fis = FileIO.openAsFileInputStream(file);
-		assertNull(fis);
-	}
-
-	@Test
-	public void testOpenReadOnlyBuffer() {
-		MappedByteBuffer mbb = FileIO.openReadOnlyBuffer(testFile);
-		byte[] data = new byte[SAMPLE_SIZE];
-		mbb.get(data);
-
-		assertArrayEquals(randomData, data);
-	}
-
-	@Test
-	public void testOpenReadOnlyBufferInvalidFile() {
-		MappedByteBuffer mbb = FileIO.openReadOnlyBuffer(new File("foo"));
-		assertNull(mbb);
-	}
-
-	@Test
-	public void testCloseFileInputStream() {
-		FileInputStream fis = FileIO.openAsFileInputStream(testFile);
-		FileIO.closeFileInputStream(fis);
-	}
-
-	@Test
 	public void testCloseFileInputStreamNull() {
 		FileIO.closeFileInputStream(null);
 	}
@@ -95,5 +49,10 @@ public class FileIOTest {
 		FileInputStream mockFis = mock(FileInputStream.class);
 		Mockito.doThrow(IOException.class).when(mockFis).close();
 		FileIO.closeFileInputStream(mockFis);
+	}
+
+	@Test
+	public void testCloseFileInputStream() throws Exception {
+		FileIO.closeFileInputStream(new FileInputStream(testFile));
 	}
 }
