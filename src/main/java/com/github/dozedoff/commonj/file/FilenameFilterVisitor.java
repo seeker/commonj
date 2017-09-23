9ebbf1588d86fdbc7b18983a1483e17e5c251bfa
Nicholas Wright <dozedoffagain@gmail.com>
2017-09-23 17:56:43 +0200

    Documentation for FilenameFilterVisitor

--- src/main/java/com/github/dozedoff/commonj/file/FilenameFilterVisitor.java
+++ src/main/java/com/github/dozedoff/commonj/file/FilenameFilterVisitor.java
@@ -13,15 +13,37 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collection;
 
+/**
+ * Convenience Visitor to find files that match the given {@link FileFilter}.
+ * 
+ * @author Nicholas Wright
+ *
+ */
 public class FilenameFilterVisitor extends SimpleFileVisitor<Path> {
 	private Collection<Path> resultOutputList;
 	private FileFilter fileFilter;
 
+	/**
+	 * Create a new Visitor that returns a list of paths that match the filter.
+	 * 
+	 * @param resultOutputList
+	 *            list where matching paths will be stored
+	 * @param fileFilter
+	 *            filter to match paths
+	 */
 	public FilenameFilterVisitor(Collection<Path> resultOutputList, FileFilter fileFilter) {
 		this.resultOutputList = resultOutputList;
 		this.fileFilter = fileFilter;
 	}
 
+	/**
+	 * Check if the file matches the provided filter.
+	 * 
+	 * @param file
+	 *            {@inheritDoc}
+	 * @param attrs
+	 *            {@inheritDoc}
+	 */
 	@Override
 	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
 		if (isAcceptedFile(file)) {
