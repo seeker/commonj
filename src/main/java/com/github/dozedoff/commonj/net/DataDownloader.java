0b73c100a2f24298849bb09d075e660ad8efc02c
Nicholas Wright <dozedoffagain@gmail.com>
2017-09-25 09:24:08 +0200

    Documentation for DataDownloader

--- src/main/java/com/github/dozedoff/commonj/net/DataDownloader.java
+++ src/main/java/com/github/dozedoff/commonj/net/DataDownloader.java
@@ -8,6 +8,19 @@ package com.github.dozedoff.commonj.net;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * Interface for downloading binary data from {@link URL}s.
+ * @author Nicholas Wright
+ *
+ */
 public interface DataDownloader {
+	/**
+	 * Download the the {@link URL} as a array of bytes.
+	 * 
+	 * @param url to download
+	 * @return a array of bytes containing the target of the {@link URL}
+	 * @throws PageLoadException if there is a error accessing the {@link URL} (not found, forbidden, ect.)
+	 * @throws IOException if there is a underlying network error
+	 */
 	byte[] download (URL url) throws PageLoadException, IOException;
 }
