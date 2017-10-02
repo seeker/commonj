6a16420c5d4b33b596bb9b0fdf06b3cb5d76e4c5
Nicholas Wright <dozedoffagain@gmail.com>
2017-10-02 18:53:38 +0200

    Documentation for Pair

--- src/main/java/com/github/dozedoff/commonj/util/Pair.java
+++ src/main/java/com/github/dozedoff/commonj/util/Pair.java
@@ -5,23 +5,57 @@
  */
 package com.github.dozedoff.commonj.util;
 
+/**
+ * Simple data structure for storing pairs of values. e.g. for nodes in trees.
+ * 
+ * @author Nicholas Wright
+ *
+ * @param <V>
+ *            left hand value
+ * @param <T>
+ *            right hand value
+ */
 public class Pair<V, T> {
 	private V left;
 	private T right;
 
+	/**
+	 * Create a new {@link Pair} with the given values
+	 * 
+	 * @param left
+	 *            left hand value
+	 * @param right
+	 *            right hand value
+	 */
 	public Pair(V left, T right) {
 		this.left = left;
 		this.right = right;
 	}
 
+	/**
+	 * Get the left hand value of this {@link Pair}
+	 * 
+	 * @return left hand value
+	 */
 	public V getLeft() {
 		return left;
 	}
 
+	/**
+	 * Get the right hand value of this {@link Pair}
+	 * 
+	 * @return right hand value
+	 */
 	public T getRight() {
 		return right;
 	}
 
+	/**
+	 * Calculate the hash value of this {@link Pair} by using the hash value
+	 * from the left and right hand side.
+	 * 
+	 * @return hash value for this object
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -31,6 +65,12 @@ public class Pair<V, T> {
 		return result;
 	}
 
+	/**
+	 * Check if two pairs are equal by comparing the value of both sides for
+	 * equality.
+	 * 
+	 * @return true if both pairs are equal, else false
+	 */
 	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean equals(Object obj) {
