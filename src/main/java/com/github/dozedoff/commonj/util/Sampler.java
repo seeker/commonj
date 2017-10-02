c17f243bbccd5187f501d139b1bb7d038bb40955
Nicholas Wright <dozedoffagain@gmail.com>
2017-10-02 18:46:12 +0200

    Documentation for Sampler

--- src/main/java/com/github/dozedoff/commonj/util/Sampler.java
+++ src/main/java/com/github/dozedoff/commonj/util/Sampler.java
@@ -11,26 +11,55 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import com.google.common.collect.EvictingQueue;
 
+/**
+ * Used for sampling values. Keeps a number of values, if the capacity is
+ * exceeded, the oldest value is discarded. Allows for mathematical operations
+ * to be performed on the stored values.
+ * 
+ * @author Nicholas Wright
+ *
+ */
 public class Sampler {
 	private EvictingQueue<Integer> ringBuffer;
 	private AtomicInteger deltaSum = new AtomicInteger();
 	private int capacity;
 
+	/**
+	 * Create a {@link Sampler} with the given size.
+	 * 
+	 * @param size
+	 *            number of values this {@link Sampler} will hold
+	 */
 	public Sampler(int size) {
 		ringBuffer = EvictingQueue.create(size);
 		capacity = size;
 	}
 
+	/**
+	 * Add the value to the current value.
+	 * 
+	 * @param delta
+	 *            value to add
+	 */
 	public void addDelta(int delta) {
 		deltaSum.addAndGet(delta);
 	}
 
+	/**
+	 * Sample the current value, adding it to the current buffer and discarding
+	 * old values if the buffer is too long. The current value is reset to 0.
+	 */
 	public void sample() {
 		synchronized (ringBuffer) {
 			ringBuffer.add(deltaSum.getAndSet(0));
 		}
 	}
 
+	/**
+	 * Calculate the average value of all values in the buffer.
+	 * 
+	 * @return the average of all values
+	 */
 	public double getAverage() {
 		synchronized (ringBuffer) {
 			if (ringBuffer.isEmpty()) {
@@ -47,6 +76,11 @@ public class Sampler {
 		}
 	}
 
+	/**
+	 * Get the current samples in the buffer.
+	 * 
+	 * @return a array containing the samples
+	 */
 	public List<Integer> getSamples() {
 		Integer[] samples;
 
@@ -58,6 +92,11 @@ public class Sampler {
 		return Arrays.asList(samples);
 	}
 
+	/**
+	 * Get the capacity of the {@link Sampler}
+	 * 
+	 * @return capacity of this {@link Sampler}
+	 */
 	public int getCapacity() {
 		return this.capacity;
 	}
