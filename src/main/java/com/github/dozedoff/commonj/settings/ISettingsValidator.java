234c60d41f994b5dd025cfb3b80467c7a0a74e86
Nicholas Wright <dozedoffagain@gmail.com>
2017-10-03 18:53:34 +0200

    Documentation for ISettingsValidator

--- src/main/java/com/github/dozedoff/commonj/settings/ISettingsValidator.java
+++ src/main/java/com/github/dozedoff/commonj/settings/ISettingsValidator.java
@@ -6,5 +6,12 @@
 package com.github.dozedoff.commonj.settings;
 
 public interface ISettingsValidator {
+	/**
+	 * Validate the passed {@link AbstractSettings} object.
+	 * 
+	 * @param settings
+	 *            to validate
+	 * @return true if the settings object passes all tests
+	 */
 	public boolean validate(AbstractSettings settings);
 }
