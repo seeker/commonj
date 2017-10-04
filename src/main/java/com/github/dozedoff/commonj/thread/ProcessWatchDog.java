330ffac2e6d22f80385402ae8ebe55d19b018cf8
Nicholas Wright <dozedoffagain@gmail.com>
2017-10-04 09:10:20 +0200

    Documentation for ProcessWatchDog

--- src/main/java/com/github/dozedoff/commonj/thread/ProcessWatchDog.java
+++ src/main/java/com/github/dozedoff/commonj/thread/ProcessWatchDog.java
@@ -8,18 +8,40 @@ package com.github.dozedoff.commonj.thread;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Class for monitoring if a {@link Process} terminates within a set time. The
+ * thread must be started and interrupted manually.
+ * 
+ * @author Nicholas Wright
+ *
+ */
 public class ProcessWatchDog extends Thread {
 	private String processDescription;
 	private Process process;
 	private long timeout;
 	private final static Logger logger = LoggerFactory.getLogger(ProcessWatchDog.class);
 
+	/**
+	 * Create a new {@link ProcessWatchDog} to monitor a {@link Process}.
+	 * 
+	 * @param processDescription
+	 *            a descriptive name, used for logging
+	 * @param process
+	 *            to monitor
+	 * @param timeout
+	 *            timeout in milliseconds
+	 */
 	public ProcessWatchDog(String processDescription, Process process, long timeout) {
 		this.process = process;
 		this.timeout = timeout;
 		this.processDescription = processDescription;
 	}
 
+	/**
+	 * Running the thread starts the {@link ProcessWatchDog}. If the thread is
+	 * not interrupted before the timeout is reached, a error is logged and the
+	 * process terminated.
+	 */
 	@Override
 	public void run() {
 		try {
