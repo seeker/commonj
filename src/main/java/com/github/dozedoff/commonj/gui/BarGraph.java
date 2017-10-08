b53db48e81b8cf92fe6e7d96034145011ebf8277
Nicholas Wright <dozedoffagain@gmail.com>
2017-10-08 18:15:22 +0200

    Documentation for BarGraph

--- src/main/java/com/github/dozedoff/commonj/gui/BarGraph.java
+++ src/main/java/com/github/dozedoff/commonj/gui/BarGraph.java
@@ -13,6 +13,12 @@ import javax.swing.JComponent;
 
 import com.github.dozedoff.commonj.util.Sampler;
 
+/**
+ * Plot data as a bar graph.
+ * 
+ * @author Nicholas Wright
+ *
+ */
 public class BarGraph implements Graph {
 	private final JComponent component;
 	private transient final Sampler sampler;
@@ -20,20 +26,51 @@ public class BarGraph implements Graph {
 	private double scaleFactor = 1;
 	private boolean autoScale = false;
 
+	/**
+	 * Create a new {@link BarGraph} on the provided component.
+	 * 
+	 * @param component
+	 *            to draw the graph on
+	 * @param sampler
+	 *            class containing the data to display
+	 */
 	public BarGraph(JComponent component, Sampler sampler) {
 		this(component, sampler, Color.BLUE);
 	}
 
+	/**
+	 * Create a new {@link BarGraph} on the provided component.
+	 * 
+	 * @param component
+	 *            to draw the graph on
+	 * @param sampler
+	 *            class containing the data to display
+	 * @param color
+	 *            Colour of the bars
+	 */
 	public BarGraph(JComponent component, Sampler sampler, Color color) {
 		this.component = component;
 		this.sampler = sampler;
 		this.barColor = color;
 	}
 
+	/**
+	 * Set if the bars height should be scaled relative to the highest bar. If
+	 * not set, the height of the graph is used, 1px = 1
+	 * 
+	 * @param autoScale
+	 *            true to enable auto-scaling, false to disable
+	 */
 	public void setAutoScale(boolean autoScale) {
 		this.autoScale = autoScale;
 	}
 
+	/**
+	 * Update the {@link BarGraph}, drawing bars for the current values.
+	 * 
+	 * @param g
+	 *            {@link Graphics} object used to draw onto the component
+	 */
 	@Override
 	public void update(Graphics g) {
 		List<Integer> graphData = sampler.getSamples();
