61d46b6559b3adc27887534f20fc29e088393b76
Nicholas Wright <dozedoffagain@gmail.com>
2017-10-08 18:53:00 +0200

    Documentation for Bits

--- src/main/java/com/github/dozedoff/commonj/util/Bits.java
+++ src/main/java/com/github/dozedoff/commonj/util/Bits.java
@@ -5,11 +5,36 @@
  */
 package com.github.dozedoff.commonj.util;
 
+/**
+ * Utility class for bit operations.
+ * 
+ * @author Nicholas Wright
+ *
+ */
 public class Bits {
+	/**
+	 * Convenience method to calculate the hamming distance string encoded long
+	 * values.
+	 * 
+	 * @param left
+	 *            value
+	 * @param right
+	 *            value
+	 * @return the hamming distance between the two values
+	 */
 	public static int hammingDistance(String left, String right) {
 		return hammingDistance(Long.parseLong(left, 2), Long.parseLong(right, 2));
 	}
 
+	/**
+	 * Calculate the hamming distance between the left and right value.
+	 * 
+	 * @param left
+	 *            value
+	 * @param right
+	 *            value
+	 * @return the hamming distance between the two values
+	 */
 	public static int hammingDistance(long left, long right) {
 		long xor = left ^ right;
 		int distance = Long.bitCount(xor);
